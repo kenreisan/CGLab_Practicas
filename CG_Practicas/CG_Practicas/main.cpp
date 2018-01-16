5abc65dce81f678822a3b49615eebe44fce61900
Eduardo Romero <eduardodrh@gmail.com>
2018-01-15 19:27:40 -0600

    Add: menu

--- CG_Practicas/CG_Practicas/main.cpp
+++ CG_Practicas/CG_Practicas/main.cpp
@@ -10,14 +10,41 @@
 #include "practica02.h"		//Cambiar el header dependiendo de la practica
 #include "practica03.h"
 
+int opcion;
+
+void menu() {
+	printf("\n  Practicas de laboratorio de computacion grafica\n");
+	printf("\n  0: Practica 02");
+	printf("\n  1: Practica 03");
+	printf("\n 99: Salir");
+	printf("\n\n  Elija la practica que desea abrir: ");
+	
+	scanf_s("%d", &opcion);
+
+	switch (opcion) {
+	
+		case 99:
+			exit(0);
+		break;
+
+		case 0:
+			inicio02();
+		break;
+
+		case 1:
+			inicio03();
+		break;
+
+		default:
+			printf("\nEsa opcion no existe...\n");
+			menu();
+		break;
+	}
+}
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);			// Inicializamos OpenGL
-
-	/*Elegimos la funcion a usar*/
-	//inicio02();
-	inicio03();
-
+	menu();
 	glutMainLoop(); 
 	return 0;
 }
